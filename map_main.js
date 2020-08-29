8792c846b58c7f0eb68cd9064474bd033266f83f
Jeancarlo Hidalgo <jeancahu@gmail.com>
2020-08-29 10:42:27 -0600

    add: Documentar el código con comentarios

--- map_main.js
+++ map_main.js
@@ -1,14 +1,18 @@
-
 // Prueba, dibujar puntos
-// generate 300 random points features
+
+// función constante para generar un número aleatorio (alias)
 const getRandomNumber = function () {
     return Math.random();
 }
 
-console.log(getRandomNumber());
-
+// se crea una lista con los diferentes puntos a dibujar sobre el mapa
 const features = [];
+
+// trecientos puntos se van a agregar a la lista
 for (i = 0; i < 300; i++) {
+
+    // cada punto varía en un poco la cordenada manteniendose
+    // en uno de los cuadrantes con centro en -85,9
     features.push(new ol.Feature({
 	geometry: new ol.geom.Point(ol.proj.fromLonLat([
 	    -85.0 + getRandomNumber(), 9.0 + getRandomNumber()
@@ -16,7 +20,8 @@ for (i = 0; i < 300; i++) {
     }));
 }
 
-// create the source and layer for random features
+// se crean los objetos vectores que serán al final de cuentas
+// lo que se dibuja sobre el objeto mapa
 const vectorSource = new ol.source.Vector({
     features
 });
@@ -31,8 +36,10 @@ const vectorLayer = new ol.layer.Vector({
     })
 });
 
-// Fin de la prueba
+// fin de la prueba de puntos aleatorios, ahora se procede con el mapa
 
+// se crea un view que es el objeto que especifica la pocisión del
+// mapa y su área así como el zoom y la delimitación
 var view = new ol.View({
     center: ol.proj.fromLonLat([-84.1027104, 9.865107]),
     zoom: 12,
@@ -40,6 +47,12 @@ var view = new ol.View({
     extent: [-9375050.54, 1092000.79, -9352512.37, 1113049.659],
 });
 
+// ahora se crea el mapa que requiere de un conjunto de capas,
+// en este caso se tiene toda la construcción de rutas y edificios
+// por defecto junto con el vector de puntos
+
+// target indica la sección del html que contendrá el
+// mapa, es decir el canvas
 var map = new ol.Map({
     layers: [
 	new ol.layer.Tile({
